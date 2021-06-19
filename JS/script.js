f11a1fdec3422cc63ed6d08f42b3363bd6206c86
Dfrmn <danikfurman2010@gmail.com>
2021-06-19 14:11:42 +0300

    home work

---
+++ JS/script.js
@@ -0,0 +1,62 @@
+let a = +prompt(`Введите значение переменной (a)`);
+alert(a === 0 ?  `Верно`: `Неверно`);
+// alert(a > 0 ? `Верно` : `Неверно`)
+// alert(a < 0 ? `Верно` : `Неверно`)
+// alert(a >= 0 ? `Верно` : `Неверно`)
+// alert(a >= 0 ? `Верно` : `Неверно`)
+// alert(a != 0 ? `Верно` : `Неверно`)
+// alert(a === `test` ? `Верно` : `Неверно`)
+// alert(a === 1 ? `Верно` : `Неверно`)
+// alert(a > 0 && a < 5 ? `Верно` : `Неверно`)
+
+// switch (a) {
+//     case 0:
+//     case 2:
+//         a += 7;
+//         alert(a);
+//         break;
+//     default:
+//         a/=10;
+//         alert(a);
+//         break;
+// }
+
+// let b = +prompt(`Введите значение переменной (б)`)
+// if (a <= 1 && b >= 3) {
+//     alert(a + b);
+// }
+// else{
+//     alert(a - b);
+// }
+
+// if (a>2 && a<11 || b>=6 && b<14) {
+//     alert(`Верно`);
+// }
+// else{
+//     alert(`Неверно`);
+// }
+
+// let num = +prompt(`Введите любое число от 1 до 4 `);
+// let result;
+// switch (num) {
+//     case 1:
+//         result = `Зима`;
+//         alert(result);
+//         break;
+//     case 2:
+//         result = `Весна`;
+//         alert(result);
+//         break;
+//     case 3:
+//         result = `Лета`;
+//         alert(result);
+//         break;
+//     case 4:
+//         result = `Осень`;
+//         alert(result);
+//         break;
+
+//     default:
+//         alert(`Вам не удалос ввести нужное число`);
+//         break;
+// }
